6287d4509cd6a57ec14e752c0fad62a7cb5723ed
Francisco Jonas <jonas.filho26@gmail.com>
2021-02-04 16:23:55 -0300

    Implementação das classes ChessException e ChessPosition.

---
+++ src/chess/exceptions/ChessException.java
@@ -0,0 +1,11 @@
+package chess.exceptions;
+
+public class ChessException extends RuntimeException {
+
+	private static final long serialVersionUID = 8954997655877517208L;
+	
+	public ChessException(String message) {
+		super(message);
+	}
+
+}
