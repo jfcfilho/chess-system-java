5ae878b1f2b9b9385023453a5df7e3a76869b6ed
Francisco Jonas <jonas.filho26@gmail.com>
2021-02-04 16:44:32 -0300

    Melhoria nas cores do tabuleiro

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -32,18 +32,18 @@ public class ChessMatch {
 	}
 	
 	private void initialSetup() {
-		//torre, bispo, cavalo, rainha, rei, cavalo, bispo, torre
-//		board.placePiece(new Rook(board, Color.BLACK), new Position(0, 0));
-//		board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
-//		board.placePiece(new Rook(board, Color.WHITE), new Position(7, 0));
-//		board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
+		placeNewPiece('c', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 1, new King(board, Color.WHITE));
 
-
-		placeNewPiece('a', 1, new Rook(board, Color.BLACK));
-		placeNewPiece('e', 1, new King(board, Color.BLACK));
-		placeNewPiece('a', 8, new Rook(board, Color.WHITE));
-		placeNewPiece('e', 8, new King(board, Color.WHITE));
-
-		
+        placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('c', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 8, new King(board, Color.BLACK));		
 	}
 }
