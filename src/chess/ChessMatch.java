7a684e00d9adc70f1f795e87791b18c42bca1623
Francisco Jonas <jonas.filho26@gmail.com>
2021-02-04 15:16:00 -0300

    Criação das classes Rook (cavalo) e King (rei) e colocação das peças no tabuleiro

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,10 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.enuns.Color;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMatch {
 
@@ -8,6 +12,7 @@ public class ChessMatch {
 
 	public ChessMatch() {
 		board = new Board(8,8);
+		initialSetup();
 	}
 
 	public ChessPiece[][] getPieces(){
@@ -23,5 +28,11 @@ public class ChessMatch {
 		return mat;
 	}
 	
-	
+	private void initialSetup() {
+		//torre, bispo, cavalo, rainha, rei, cavalo, bispo, torre
+		board.placePiece(new Rook(board, Color.BLACK), new Position(0, 1));
+		board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
+		board.placePiece(new Rook(board, Color.WHITE), new Position(7, 1));
+		board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
+	}
 }
---
+++ src/chess/pieces/King.java
@@ -0,0 +1,17 @@
+package chess.pieces;
+
+import boardgame.Board;
+import chess.ChessPiece;
+import chess.enuns.Color;
+
+public class King extends ChessPiece {
+
+	public King(Board board, Color color) {
+		super(board, color);
+	}
+	
+	@Override
+	public String toString() {
+		return "K";
+	}
+}
