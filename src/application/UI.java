bc1e5170b7a7327ab2d5cf69d90c2bf6a397f0b8
Francisco Jonas <jonas.filho26@gmail.com>
2021-02-05 18:37:35 -0300

    Tratamento de exceções no programa principal e limpeza da tela

--- src/application/UI.java
+++ src/application/UI.java
@@ -66,4 +66,10 @@ public class UI {
 			throw new InputMismatchException("Erro lendo posição: Os valores válidos são de a1 a h8!");
 		}
 	}
+	
+	// hhtps://stackoverflow.com/questions/2979383/java-clear-the-console
+	public static void clearScreen() {
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}
 }
